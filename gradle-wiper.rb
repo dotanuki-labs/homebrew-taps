fe139407e08dd4f37f38f482e8eb141808bdcd7a
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2024-08-10 13:39:33 +0200

    Bumps gradle-wiper to v0.2.0

--- gradle-wiper.rb
+++ gradle-wiper.rb
@@ -4,7 +4,7 @@
 class GradleWiper < Formula
   desc "Easily reclaim machine resources (RAM, Disk) taken by Gradle builds"
   homepage "https://github.com/dotanuki-labs/gradle-wiper"
-  version "0.1.0"
+  version "0.2.0"
 
   os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
   arch = case Hardware::CPU.arch
@@ -19,10 +19,10 @@ class GradleWiper < Formula
   @@using = :nounzip
 
   @@sha256 = case "#{arch}-#{os}"
-    when "x86_64-unknown-linux-gnu" then "66cdb629934e4b114520685260ce7074f75702f9a92de820b0a5da0fd1678d00"
-    when "x86_64-apple-darwin" then "307e40c054502b656d568b306a397c6ae79f57ecc7cf0a559effab3f36624a02"
-    when "aarch64-unknown-linux-gnu" then "10c51bbb0613bfc45bb7a9f0e56500031a166e432e9f8449dc1f2f0af45a7adb"
-    when "aarch64-apple-darwin" then "72aecebcd5fe423d6de2f59c0e73102e31d14f0b765beb2bac94e5f830c382c4"
+    when "x86_64-unknown-linux-gnu" then "344fb7b1493426094f232af0f97f99bf623ea88e5532da026bbd7f70c3e8ddd3"
+    when "x86_64-apple-darwin" then "3ca994e699062fbcba836f7edfc0e41a8b8e794fbdc9b1ed1abf3763c5760f9d"
+    when "aarch64-unknown-linux-gnu" then "54b5ebdbac149598b4087292ae66385239672e23226d46e4f319443295578870"
+    when "aarch64-apple-darwin" then "d47b9a4222327746b489cefc5acb3bab6eaabe1f479da387dd59c4f0f82304e5"
     else
       raise "gradle-wiper: Unsupported target : #{arch}-#{os}"
     end
