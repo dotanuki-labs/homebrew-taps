a22a012ab1149bcfee12bd6c6af072060e14a5d6
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2025-05-23 00:01:36 +0200

    Bumps gwv to 0.4.0

--- gwv.rb
+++ gwv.rb
@@ -4,7 +4,7 @@
 class Gwv < Formula
   desc "A validator for gradle/wrapper jar binaries, intended to be used in CI pipelines"
   homepage "https://github.com/dotanuki-labs/gradle-wrapper-validator"
-  version "0.3.0"
+  version "0.4.0"
 
   os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
   arch = case Hardware::CPU.arch
@@ -19,10 +19,10 @@ class Gwv < Formula
   @@using = :nounzip
 
   @@sha256 = case "#{arch}-#{os}"
-    when "x86_64-unknown-linux-gnu" then "398c04f2fa3f92012246e1ee7952bd185a40c9e7df11ca72b97e099905f211e9"
-    when "aarch64-unknown-linux-gnu" then "6564fb33ccac01e793c6bf50a3ea6a1c100e482766577a8b3b219a004926f233"
-    when "x86_64-apple-darwin" then "41be9f5e890025d8eb4d71385f43b8170902aa6e099a6315c296fec1c50d6cc1"
-    when "aarch64-apple-darwin" then "ad21188f434ae1778ffff2d812389465295dc38528b48574c91680c8a1355325"
+    when "x86_64-unknown-linux-gnu" then "6b97d329c76e9f288a51ab490739c63520b6b581c69f216db443e8f39011784c"
+    when "aarch64-unknown-linux-gnu" then "9e115c1b70680b58cc8db30172184f9019f5a42b5d800c5ed60e3e8b8597c44b"
+    when "x86_64-apple-darwin" then "c46422dcafc35b6d15b7d84362b203653243fe6ebf8b46daf99beee7058dc9ce"
+    when "aarch64-apple-darwin" then "0bc35c5920cf55b26a56285a2e46035838dc82cae1371fe469790aa5270b6d2e"
     else
       raise "gwv: Unsupported combination (arch / operating system) #{arch}-#{os}"
     end
